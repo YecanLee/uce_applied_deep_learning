1c1d42603f54dc2b328e7a123e56eb7749b2694f
Meimingwei Li <yecanlee@yahoo.com>
2024-02-26 14:30:57 +0100

    Update Model Test Notebook

--- tests/test_uce_model.py
+++ tests/test_uce_model.py
@@ -25,4 +25,34 @@ def test_layer_collection():
     assert len(model.ca_layers) > 0  
 
 
+def test_model_config_stable():
+    """
+    Compare if the model configuration file is stable between different Stable Diffusion Models
+    """
+    original_model = UnifiedConceptEditor(
+        type='UnifiedConceptEditor',
+        stable_diffusion='stabilityai/stable-diffusion-1-5-base',
+        lamb=0.1,
+        erase_scale=1.0,
+        preserve_scale=0.1,
+        with_to_k=True,
+        device='cuda:0'
+    )
+    state_dict, meta_info = original_model.state_dict()
+    new_model = UnifiedConceptEditor(
+        type='UnifiedConceptEditor',
+        stable_diffusion='stabilityai/stable-diffusion-2-1-base',
+        lamb=0.1,
+        erase_scale=1.0,
+        preserve_scale=0.1,
+        with_to_k=True,
+        device='cuda:0'
+    )
+    new_model.load_state_dict(state_dict)
+
+    assert new_model.lamb == original_model.lamb, "The Lambd value is mismatched"
+    assert new_model.erase_scale == original_model.erase_scale, "The erase scale value is mismatched"
+    assert new_model.preserve_scale == original_model.preserve_scale, "The preserve scale value is mismatched"
+    assert new_model.with_to_k == original_model.with_to_k, "The with_to_k value is mismatched"
+    assert new_model.device == original_model.device, "The device value is mismatched, you need to put every model to the same device to compare the performance of the models"
 
